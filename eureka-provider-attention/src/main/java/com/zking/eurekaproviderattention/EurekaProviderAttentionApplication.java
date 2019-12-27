208703241adc6b104b0a783b38a63f2f504769ac
hx <1518137807@qq.com>
2019-12-27 08:44:03 +0800

    是否关注模块

---
+++ eureka-provider-attention/src/main/java/com/zking/eurekaproviderattention/EurekaProviderAttentionApplication.java
@@ -0,0 +1,13 @@
+package com.zking.eurekaproviderattention;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaProviderAttentionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaProviderAttentionApplication.class, args);
+	}
+
+}
---
+++ eureka-provider-attention/src/test/java/com/zking/eurekaproviderattention/EurekaProviderAttentionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zking.eurekaproviderattention;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaProviderAttentionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
